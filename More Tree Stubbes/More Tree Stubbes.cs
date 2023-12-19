d6ef4023d33b54203daa036ea22ba37745755e9b
jneb802 <73610029+jneb802@users.noreply.github.com>
2023-12-19 13:30:19 -0800

    commit

--- More Tree Stubbes/More Tree Stubbes.cs
+++ More Tree Stubbes/More Tree Stubbes.cs
@@ -8,15 +8,15 @@ using System.IO;
 using System.Reflection;
 using System.Collections.Generic;
 
-namespace oakStubbe
+namespace moreTreeStubbes
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
     //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
-    internal class oakStubbe : BaseUnityPlugin
+    internal class moreTreeStubbes : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.bepinex.oakStubbe";
-        public const string PluginName = "Oak Stubbe";
+        public const string PluginGUID = "com.bepinex.moreTreeStubbes";
+        public const string PluginName = "More Tree Stubbes";
         public const string PluginVersion = "0.0.1";
 
         // Use this class to add your own localization to the game
@@ -29,7 +29,7 @@ namespace oakStubbe
         private void Awake()
         {
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
-            Jotunn.Logger.LogInfo("Oak Stubbe has Loaded");
+            Jotunn.Logger.LogInfo("More Tree Stubbes has Loaded");
 
             // Subscribe to the OnVanillaPrefabsAvailable event
             PrefabManager.OnVanillaPrefabsAvailable += OnPrefabsAvailable;
@@ -54,7 +54,7 @@ namespace oakStubbe
             oakStumpPrefab = embeddedResourceBundle.LoadAsset<GameObject>("oak_stubbe"); // Replace with your actual asset name
 
             // Print Embedded Resources
-            Jotunn.Logger.LogInfo($"Embedded resources: {string.Join(", ", typeof(oakStubbe).Assembly.GetManifestResourceNames())}");
+            Jotunn.Logger.LogInfo($"Embedded resources: {string.Join(", ", typeof(moreTreeStubbes).Assembly.GetManifestResourceNames())}");
 
             if (embeddedResourceBundle == null)
             {
@@ -80,7 +80,7 @@ namespace oakStubbe
             // Configure the Destructible component
             var destructible = oakStumpPrefab.GetComponent<Destructible>() ?? oakStumpPrefab.AddComponent<Destructible>();
             destructible.m_minToolTier = 2;
-            destructible.m_health = 100f;
+            destructible.m_health = 120f;
 
             // Set up destroyed and hit effects
             GameObject destroyedEffectPrefab = PrefabManager.Cache.GetPrefab<GameObject>("vfx_stubbe");
@@ -109,33 +109,37 @@ namespace oakStubbe
                     m_item = PrefabManager.Instance.GetPrefab("Wood"),
                     m_stackMin = 4,
                     m_stackMax = 6,
-                    m_weight = 1f
+                    m_weight = 0.75f
                 },
 
                 new DropTable.DropData
                 {
                     m_item = PrefabManager.Instance.GetPrefab("FineWood"),
-                    m_stackMin = 4, // Minimum amount of Fine wood
-                    m_stackMax = 5, // Maximum amount of Fine wood
-                    m_weight = 1f // Adjust the weight as needed (lower weight means it's rarer)
+                    m_stackMin = 6,
+                    m_stackMax = 10,
+                    m_weight = 1f
                 }
 
             };
+
+            dropOnDestroyed.m_dropWhenDestroyed.m_dropMin = 2;
 
             // Define the vegetation configuration
             VegetationConfig oakStumpConfig = new VegetationConfig
             {
                 Biome = Heightmap.Biome.Meadows | Heightmap.Biome.Swamp | Heightmap.Biome.BlackForest | Heightmap.Biome.Mistlands,
                 BlockCheck = true,
-                Min = 2,
-                Max = 3,
+                Min = 0,
+                Max = 2,
                 InForest = true,
                 ForestThresholdMin = 0,
                 ForestThresholdMax = 2,
                 MaxTilt = 20,
-                MaxTerrainDelta = 2,
-                TerrainDeltaRadius = 4,
-                GroupSizeMax = 1
+                MaxTerrainDelta = 3,
+                TerrainDeltaRadius = 6,
+                GroupSizeMax = 1,
+                GroundOffset = -0.4f,
+                
             };
 
             // Create and add the custom vegetation
